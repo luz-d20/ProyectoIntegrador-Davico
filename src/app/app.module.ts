e09a0d27b01258d3ec5dcd8f6a14ce2ddcf6c918
Luz Davico <mldavico@gmail.com>
2022-08-07 17:10:01 -0300

    Last sections + styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,11 @@ import { AboutComponent } from './components/about/about.component';
 import { WorkExpComponent } from './components/work-exp/work-exp.component';
 import { EducationComponent } from './components/education/education.component';
 
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,11 +25,22 @@ import { EducationComponent } from './components/education/education.component';
     HeroComponent,
     AboutComponent,
     WorkExpComponent,
-    EducationComponent
+    EducationComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: "#78C000",
+      innerStrokeColor: "#C7E596",
+      animationDuration: 300
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
