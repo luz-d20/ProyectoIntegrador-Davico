6fc64300e5c1cf7fac1cbddaa02f2553ba83c198
Luz Davico <mldavico@gmail.com>
2022-08-07 11:36:56 -0300

    Header components & styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArgProgComponent } from './components/logo-arg-prog/logo-arg-prog.component';
+import { RrssComponent } from './components/rrss/rrss.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgProgComponent,
+    RrssComponent,
+    HeroComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
