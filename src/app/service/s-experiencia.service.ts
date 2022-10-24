2b6a2a5e2d9e823d89019d463e794a3616a7b5d8
Luz Davico <mldavico@gmail.com>
2022-10-23 22:09:35 -0300

    Firebase connection

--- src/app/service/s-experiencia.service.ts
+++ src/app/service/s-experiencia.service.ts
@@ -8,7 +8,7 @@ import { Experiencia } from '../model/experiencia';
 })
 export class SExperienciaService {
 
-  expUrl = 'http://localhost:8080/explab/';
+  expUrl = 'https://backend-argprog-davico.herokuapp.com/explab/';
 
   constructor(private httpClient: HttpClient) { }
 
